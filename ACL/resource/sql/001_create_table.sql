6614e866f1edf1c2b80e6e03b7dde0c3a87b72ea
mayur61196 <45622994+mayur61196@users.noreply.github.com>
2020-05-04 14:26:03 +0530

    Add files via upload

---
+++ ACL/resource/sql/001_create_table.sql
@@ -0,0 +1,81 @@
+USE ACL;
+
+CREATE TABLE IF NOT EXISTS user(
+    user_id             INT         PRIMARY KEY,
+    first_name          CHAR(30)    NOT NULL,
+    last_name           CHAR(30)    NOT NULL,
+    password            VARBINARY(128)    NOT NULL,
+    user_creation_date       DATETIME    DEFAULT CURRENT_TIMESTAMP
+)
+
+CREATE TABLE IF NOT EXISTS group(
+    group_id            INT         PRIMARY KEY,
+    group_name          CHAR(30)    NOT NULL,            
+    group_creation_date       DATETIME    DEFAULT CURRENT_TIMESTAMP
+)
+
+CREATE TABLE IF NOT EXISTS file (
+    file_id             INT         PRIMARY KEY,
+    file_name           CHAR(30)    NOT NULL,
+    file_path           CHAR(100)   NOT NULL 
+)
+
+
+CREATE TABLE IF NOT EXISTS folder (
+    folder_id             INT         PRIMARY KEY,
+    folder_name           CHAR(100)   NOT NULL,
+    folder_path           CHAR(100)   NOT NULL
+)
+
+
+CREATE TABLE IF NOT EXISTS permission (
+    permission_id       INT         PRIMARY KEY,
+    per_type            CHAR(30)    NOT NULL
+)
+
+
+CREATE TABLE IF NOT EXISTS user_group_mapping (
+    user_id             INT,
+    group_id            INT,
+    PRIMARY KEY(user_id,group_id),
+    FOREIGN KEY(user_id) 
+       REFERENCES user(user_id),
+    FOREIGN KEY(group_id) 
+       REFERENCES group(group_id)
+)
+
+
+CREATE TABLE IF NOT EXISTS file_folder_mapping (
+    folder_id           INT,
+    file_id             INT,
+    PRIMARY KEY(folder_id,file_id),
+    FOREIGN KEY(folder_id) 
+       REFERENCES folder(folder_id),
+    FOREIGN KEY(file_id) 
+       REFERENCES file(file_id),
+    )
+
+CREATE TABLE IF NOT EXISTS folder_mapping(
+    parent_folder_id    INT,
+    child_folder_id     INT,
+    PRIMARY KEY(parent_folder_id,child_folder_id),
+    FOREIGN KEY(parent_folder_id) 
+       REFERENCES folder(folder_id),
+    FOREIGN KEY(child_folder_id) 
+       REFERENCES folder(folder_id)
+    )
+
+CREATE TABLE IF NOT EXISTS file_permission(
+    user_id             INT,
+    file_id             INT,
+    permission_id       INT,
+    PRIMARY KEY(user_id,file_id),
+    FOREIGN KEY(user_id) 
+       REFERENCES user(user_id),
+    FOREIGN KEY(file_id) 
+       REFERENCES file(file_id),
+    FOREIGN KEY(permission_id) 
+       REFERENCES permission(permission_id)
+)
+
+
